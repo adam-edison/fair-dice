c78700ab8d4988e37fb4a8b773b35d78dc2e353b
Adam Edison <adam.edison@gmail.com>
2023-02-09 00:47:27 -0500

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4MzQyNQ/spectral.js?branch_slug=master&token=46e5f052-4805-45f8-97a1-463106cc9816";
+export default { extends: ruleset };
